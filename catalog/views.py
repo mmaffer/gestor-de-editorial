f45a30469e13bc1380552394b0a08f43f4919bb3
Mafer <maria.moya@tecsup.edu.pe>
2025-10-15 23:31:26 -0500

    Implementar ViewSets para operaciones CRUD

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# catalog/views.py
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from .models import Editorial, Libro
+from .serializers import EditorialSerializer, LibroSerializer
+
+# 📚 CRUD básico de Entidad 2 (Editorial)
+class EditorialViewSet(viewsets.ModelViewSet):
+    queryset = Editorial.objects.all()
+    serializer_class = EditorialSerializer
+
+# 📃 Listado, ➕ Creación, ✏️ Edición, ❌ Eliminación y 🔍 Búsqueda de Entidad 1 (Libro)
+class LibroViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
+    
+    # 🔍 Implementación de búsqueda por filtros
+    filter_backends = [filters.SearchFilter]
+    # Campos por los que el cliente podrá buscar
+    search_fields = ['titulo', 'genero']
